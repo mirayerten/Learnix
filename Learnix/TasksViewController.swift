050f7213b40d4b9974c1c789e6e1dbb842b761d9
Miray Yılmaz <mirayertenn@gmail.com>
2025-05-17 15:39:36 +0300

    TasksViewController and updates

---
+++ Learnix/TasksViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TasksViewController.swift
+//  Learnix
+//
+//  Created by Miray Erten on 16.05.2025.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
